3ec566d4a625f164b62bff8a60153a53dc21e53c
Jan Parisek <64596848+janparisek@users.noreply.github.com>
2020-09-14 02:40:36 +0200

    Create fixme.sh

---
+++ fixme.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+printf "Repo link?:.\n";
+read repo;
+
+git clone --bare $repo.git;
+
+echo `expr $repo : "https:\\/\\/github.com\\/.+\\/\(.*\)\\.git"`;
+
+cd
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Your Correct Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git push --force --tags origin 'refs/heads/*'
